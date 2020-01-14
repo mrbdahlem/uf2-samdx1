f5f9456cc91c2df9e6e9cfe48e6cbe680d886417
Brian Dahlem <mrbdahlem@gmail.com>
2020-01-13 19:40:52 -0700

    added sugarcookie board

---
+++ boards/sugarcookie/board_config.h
@@ -0,0 +1,14 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Brian Dahlem"
+#define PRODUCT_NAME "SugarCookie"
+#define VOLUME_LABEL "SUGARBOOT"
+#define BOARD_ID "SAMD21E18A-SugarCookie-v0"
+#define INDEX_URL "https://mrbdahlem.github.io/SugarCookie/"
+#define USB_VID 0x16D0	 // MCS Electronics
+#define USB_PID 0x0F3C   // Generic HID device
+
+#define LED_PIN PIN_PA27
+
+#endif
